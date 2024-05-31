7ba6abd8230e2349f64be34b706ba9b91549ec16
ackimixs <ackimixs@protonmail.com>
2024-05-31 11:51:59 +0200

    files

---
+++ item.py
@@ -0,0 +1,15 @@
+
+class Item:
+    def __init__(self, name, mass, utility):
+        self.name = name
+        self.mass = mass
+        self.utility = utility
+
+    def __str__(self):
+        return f'{self.name} ({self.mass} kg, {self.utility} utils)'
+
+    def __repr__(self):
+        return f'Item({self.name}, {self.mass}, {self.utility})'
+
+    def __eq__(self, other):
+        return self.name == other.name and self.mass == other.mass and self.utility == other.utility
--- main.py
+++ main.py
@@ -1,15 +1,3 @@
+from item import Item
 
-class Item:
-    def __init__(self, name, mass, utility):
-        self.name = name
-        self.mass = mass
-        self.utility = utility
-
-    def __str__(self):
-        return f'{self.name} ({self.mass} kg, {self.utility} utils)'
-
-    def __repr__(self):
-        return f'Item({self.name}, {self.mass}, {self.utility})'
-
-    def __eq__(self, other):
-        return self.name == other.name and self.mass == other.mass and self.utility == other.utility
+item_list = []
