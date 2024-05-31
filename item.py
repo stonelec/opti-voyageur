f1d343df26373784a35b1b80afb7d8d7d52d9906
ackimixs <ackimixs@protonmail.com>
2024-05-31 14:13:46 +0200

    csv

--- item.py
+++ item.py
@@ -1,3 +1,5 @@
+import time
+
 
 class Item:
     def __init__(self, name, mass, utility):
--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 from item import Item
 
-item_list = []
+
+def read_csv(filename):
+    item_list = []
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                name, mass, utility = line.strip().split(';')
+                item = Item(name, float(mass), float(utility))
+                item_list.append(item)
+    except FileNotFoundError:
+        print(f'File {filename} not found')
+
+    return item_list
+
+
+def main():
+    item_list = read_csv('velo.csv')
+
+    for item in item_list:
+        print(item)
+
+
+if __name__ == '__main__':
+    main()
