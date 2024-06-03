3fd85d01868bc0b659c3c77a21681b67a3f51644
ackimixs <ackimixs@protonmail.com>
2024-06-03 16:41:20 +0200

    refractor

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-from item import Item, exact, heuristic_1, heuristic_2
+from item import Item, exact, heuristic_1, heuristic_2, read_csv
 import time
-
-
-def read_csv(filename):
-    item_list = []
-    try:
-        with open(filename, 'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                name, mass, utility = line.strip().split(';')
-                item = Item(name, float(mass), float(utility))
-                item_list.append(item)
-    except FileNotFoundError:
-        print(f'File {filename} not found')
-
-    return item_list
-
 
 def main():
     item_list = read_csv('velo_1.csv')
