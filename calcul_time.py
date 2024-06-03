7cb36afcd6645c18e77b7d378bf80ff43975b665
vincent <3millestone@gmail.com>
2024-06-03 14:18:03 +0200

    add calcul time functions

---
+++ calcul_time.py
@@ -0,0 +1,17 @@
+import time
+
+def operation():
+    return 69 + 420
+
+def mesure(n):
+    start_time = time.time()
+    for i in range(n):
+        operation()
+    end_time = time.time()
+    duration = end_time - start_time
+    return duration / n
+
+print(f"{mesure(100000):.10f} secondes/operation")
+
+
+
